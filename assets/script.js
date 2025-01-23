e6c77ba1d0d11da8de5a275c29d6ca7be9d2cd00
AnnaSilverIT <student291297@gmail.com>
2025-01-23 20:31:36 +0300

    change styles, add clearButton

--- assets/script.js
+++ assets/script.js
@@ -1,59 +1,35 @@
-const btn = document.querySelector('.button');
+const addBtn = document.getElementById('addButton');
+const clearBtn = document.getElementById('clearButton');
 const result = document.querySelector('.ul');
 const input = document.querySelector('#input');
+const noTask = document.querySelector('.noTask')
 
-
-btn.onclick = () => {
+addBtn.onclick = () => { 
+  if (input.value != false){
   let newTask = document.createElement('li');
   newTask.className = 'li'
   newTask.innerText = input.value
   input.value = ''
   result.appendChild(newTask)
 
-  let close = document.createElement('input')
-  close.type = 'checkbox'
-  close.className = 'done'
-  newTask.appendChild(close)
+  let taskDone = document.createElement('input')
+  taskDone.type = 'checkbox'
+  taskDone.className = 'done'
+  newTask.appendChild(taskDone)
   newTask.style.cursor = 'pointer'
-  close.onclick = () =>{
-    result.removeChild(newTask)
-  }
+  noTask.style.display = 'none'
+  clearBtn.disabled = false
+  // localStorage.setItem('key', JSON.stringify(input.value))
+  let inpValue = input.value
+  let arr = {text: inpValue}
+  localStorage.setItem('taskslist', JSON.stringify(arr))
+}
 }
 
 
-// btn.addEventListener('click', (e) => {
-//   // if (input.value === '') return
-//   createDeleteElements(input.value)
-//   input.value =''
-// })
-
-// function createDeleteElements(value) {
-//   const li= document.createElement('li');
-//   li.className = "li";
-//   li.textContent = value
-//   result.appendChild(li)
-
-//   const btn = document.createElement('button');
-//   btn.className = "button";
-//   btn.textContent = 'done'
-//   li.appendChild(btn)
-// }
-
-
-
-// function addTask(e) {
-//   e.preventDefault();
-//   let inputText = input.value;
-//   let li= document.createElement('li');
-//   li.className = "li";
-//   let textNode = document.createTextNode(inputText)
-//   ul.appendChild(textNode);
-//    ul.prepend(li)
-//   //
-
-//   let checkbox = document.createElement('input')
-//   checkbox.type = 'checkbox'
-//   checkbox.addEventListener('click', function(e){
-    
-//   })
-// }
+clearBtn.addEventListener('click' , function(e) {
+  e.preventDefault
+  noTask.style.display = 'flex'
+  result.innerHTML = ''
+clearBtn.disabled= true
+}) 
